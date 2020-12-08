32e7733c14fee9374816b75243d1303a833b719d
nhoanglong20 <hoanglongmcs1@gmail.com>
2020-12-09 05:08:33 +0700

    fix upload async

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,12 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-exports.getUserProfile = async(req, res, next) => {
-    let user = await User.findById("5fcef6e138e0b6cd47a3d49a");
+exports.getUserProfile = async (req, res, next) => {
+    let user = await User.findById('5fcef6e138e0b6cd47a3d49a');
     res.render('user', { user });
 };
 
-exports.changeInfo = async(req, res) => {
+exports.changeInfo = async (req, res) => {
     //var product = req.body.product;
     //console.log(product);
     const form = new formidable.IncomingForm();
@@ -22,19 +22,19 @@ exports.changeInfo = async(req, res) => {
     form.uploadDir = path.join(__dirname, '/../uploads');
     form.keepExtensions = true;
     form.maxFieldsSize = 10 * 1024 * 1024; //10MB
-    form.parse(req, async(err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {
         if (err) {
             return;
         }
-        console.log(files)
+        console.log(files);
         const uploadedPath = files.images.path;
         const uploadedRes = await cloudinary.uploader.upload(uploadedPath);
-        let user = await User.findOneAndUpdate({ _id: "5fcef6e138e0b6cd47a3d49a" }, { coverImage: uploadedRes.secure_url });
+        let user = await User.findOneAndUpdate(
+            { _id: '5fcef6e138e0b6cd47a3d49a' },
+            { coverImage: uploadedRes.secure_url }
+        );
 
-        fs.unlink(uploadedPath, function(err) {
-            if (err) throw err;
-            console.log('File deleted!');
-        });
+        fs.unlinkSync(uploadedPath);
         console.log('Uploaded product successfully');
         res.redirect('/user');
     });
