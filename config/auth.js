99a12286fc22b386891c2ce311cf8db1c0138d1f
nhoanglong20 <hoanglongmcs1@gmail.com>
2021-01-08 13:56:52 +0700

    fix deployment bug

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
     googleAuth: {
         clientID:
-            '981888513888-pjlqpg3q99gcj9cpjc8epd7ko0anbb97.apps.googleusercontent.com',
-        clientSecret: 'OhegKEUXsT-x5HwXII78dS1i',
+            '574477827788-o7gkom3f7c6k9o5ioc6f7nqn8n78jsjc.apps.googleusercontent.com',
+        clientSecret: 'rMf3dTI7IoAmIc2Fs4aQnzJ6',
         callbackURL: `http://${process.env.HOST_URL}:${
             process.env.PORT || 3000
         }/auth/google/callback`
