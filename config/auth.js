bb100b21d7dc5c1bf9162220f8cf39c82a96f7b2
nhoanglong20 <hoanglongmcs1@gmail.com>
2021-01-08 13:48:54 +0700

    fix env for deployment

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,10 @@
 module.exports = {
- googleAuth: {
-   clientID: '981888513888-pjlqpg3q99gcj9cpjc8epd7ko0anbb97.apps.googleusercontent.com',
-   clientSecret: 'OhegKEUXsT-x5HwXII78dS1i',
-   callbackURL: 'http://localhost:3000/auth/google/callback',
- },
+    googleAuth: {
+        clientID:
+            '981888513888-pjlqpg3q99gcj9cpjc8epd7ko0anbb97.apps.googleusercontent.com',
+        clientSecret: 'OhegKEUXsT-x5HwXII78dS1i',
+        callbackURL: `http://${process.env.HOST_URL}:${
+            process.env.PORT || 3000
+        }/auth/google/callback`
+    }
 };
